cb897c22474a85e1e38fdbe1b847251dfab81884
Aryan Garg <aryangarg@Aryan-Mac-M1.local>
2023-03-15 12:39:14 +0530

    TripGenie

---
+++ downloads/FlutterTravel-master/lib/util/places.dart
@@ -0,0 +1,77 @@
+List places = [
+  {
+    "name": "Kabini, Mysore",
+    "img": "assets/mysore.jpg",
+    "price": r"$100/person",
+    "location": "Karnataka",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\n\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Manali",
+    "img": "assets/manali.jpg",
+    "price": r"$100/person",
+    "location": "Himachal",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Panaji",
+    "img": "assets/goa.jpeg",
+    "price": r"$100/person",
+    "location": "Goa",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\n\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Connaught Place",
+    "img": "assets/cp.jpg",
+    "price": r"$100/person",
+    "location": "New Delhi",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Jaipur",
+    "img": "assets/jaipur.jpg",
+    "price": r"$100/person",
+    "location": "Rajasthan",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+];
